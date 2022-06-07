bae4e6c154cc933d0d5c6ecc6558b9e11cebf402
Nurbolot <ngulamidinov@gmail.com>
2022-06-07 13:35:15 +0600

    add: usefulFutires - MathN - pow, powWithFor RU: В пакет usefulFutires добавлен класс MathN и методы pow, powWithFor

---
+++ src/main/java/useFulFutires/MathN.java
@@ -0,0 +1,44 @@
+package useFulFutires;
+
+public class MathN {
+    public static double pow(int a, int b){
+        if (a > 0 && b == 0){
+            return 1;
+        }
+        if (a == 0){
+            return 0;
+        }
+
+        double result = 1;
+        if (a > 0 && b > 0){
+            result = powWithFor(a, b);
+            return result;
+            // to do Fast
+            /*if(b % 2 == 0){
+                result = powWithFor(a, b);
+                return result;
+            }
+            else {
+                result = powWithFor(a, b);
+                return result;
+            }*/
+        }
+
+        else if (a > 0 && b < 0){
+            b *= -1;
+            result = powWithFor(a, b);
+            result = 1 / result;
+            return result;
+        }
+        return result;
+    }
+
+    // to do Fast
+    public static double powWithFor(int a, int b){
+        double result = 1;
+        for (int i = 0; i < b; i++){
+            result *= a;
+        }
+        return result;
+    }
+}
