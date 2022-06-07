f921a35378d823058dc35af3cd5365c7a19a1d5f
Nurbolot <ngulamidinov@gmail.com>
2022-06-07 13:33:47 +0600

    add: usefulFutires - FileN - BufferedReader, Writer RU: В пакет usefulFutires добавлен класс FileN и методы BufferedReader, Writer

---
+++ src/main/java/useFulFutires/FileN.java
@@ -0,0 +1,44 @@
+package useFulFutires;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileN {
+    public static String BufferedReader(String path) throws IOException {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String str = null;
+            StringBuilder stringBuilder = new StringBuilder();
+            String nLine = "\n";
+
+            while ((str = reader.readLine()) != null) {
+                stringBuilder.append(str);
+                stringBuilder.append(nLine);
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            reader.close();
+            return stringBuilder.toString();
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Не удалось прочитат файл!");
+            return "";
+        }
+    }
+
+    public static void Writer(String str, String path, boolean f){
+        try{
+            FileWriter fw = new FileWriter(path, f);
+            fw.write(String.valueOf(str));
+            fw.append("\n");
+            fw.flush();
+            System.out.println("File Frited");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Ошибка при записи файла");
+        }
+    }
+}
